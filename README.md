656c4d6a1302d20c76cb5eb300231baf09f70087
vampirefan <vampirefan@126.com>
2022-10-16 09:30:21 +0800

    code format & update README

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pack-util
+打包工具，可以将 ./node_modules 中安装的包遍历后打包，并放在 ./node_modules_pack 文件夹中，然后批量发布到 nexus 搭建的 npm 私服上。
 Pack all installed package in ./node_modules, and publish to Nexus hosted Registry.
 
 ## Install
--- bin/index.js
+++ bin/index.js
@@ -1,60 +1,57 @@
 #!/usr/bin/env node
 
-import { execa } from 'execa';
+import { execa } from 'execa'
 import { mkdirSync, readdirSync } from 'fs'
-import { Command } from 'commander';
+import { Command } from 'commander'
 
-const program = new Command();
+const program = new Command()
 
 const getPackageNames = async () => {
-  const { stdout } = await execa('npm', ['list','--all']);
+  const { stdout } = await execa('npm', ['list', '--all'])
   let packages = stdout.split('\n')
-    .filter(item=>!item.includes('deduped'))//去掉重复包
-    .map(name=> name.substring(name.lastIndexOf(' ')+1))//解析每行的包名称
-    .filter(packageName=>packageName.length>0)//去掉空行
-  packages.splice(0,1)//去掉首行路径字符串
+    .filter(item => !item.includes('deduped'))//去掉重复包
+    .map(name => name.substring(name.lastIndexOf(' ') + 1))//解析每行的包名称
+    .filter(packageName => packageName.length > 0)//去掉空行
+  packages.splice(0, 1)//去掉首行路径字符串
   return packages
-};
+}
 
-const packPackage= async(packageName)=>{
-  const {stdout} = await execa('npm',['pack',packageName,'--pack-destination=./node_modules_pack'])
+const packPackage = async (packageName) => {
+  const { stdout } = await execa('npm', ['pack', packageName, '--pack-destination=./node_modules_pack'])
   return stdout
 }
 
-const publishPackage = async(packageName)=>{
-  const {stdout} = await execa('npm',['publish','./node_modules_pack/'+packageName])
+const publishPackage = async (packageName) => {
+  const { stdout } = await execa('npm', ['publish', './node_modules_pack/' + packageName])
   return stdout
 }
 
 program
   .name('pack-util')
   .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
-  .version('1.0.4');
+  .version('1.0.4')
 
 program
   .command('pack')
   .description('Pack all installed package in ./node_modules')
   .action(() => {
-    getPackageNames().then(packages=>{
+    getPackageNames().then(packages => {
       console.log(`Total ${packages.length} packages to be packed, please wait...`)
-      mkdirSync('./node_modules_pack',{recursive:true})
+      mkdirSync('./node_modules_pack', { recursive: true })
       packages.forEach(packageName => {
-        packPackage(packageName).then(stdout=>console.log(stdout))
-      });
+        packPackage(packageName).then(stdout => console.log(stdout))
+      })
     })
   })
 
 program
   .command('publish')
   .description('Publish all packed packages in ./node_modules_pack')
-  .action(()=>{
+  .action(() => {
     const packages = readdirSync('./node_modules_pack')
-    // console.log('Publish all packed packages in ./node_modules_pack')
-    // console.log(packages)
     packages.forEach(packageName => {
-      publishPackage(packageName).then(stdout=>console.log(stdout))
+      publishPackage(packageName).then(stdout => console.log(stdout))
     })
   })
 
-
-program.parse();
+program.parse()
