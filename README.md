a9df39ddf08054076071572689036b457a33bac1
vampirefan <vampirefan@126.com>
2022-10-17 14:28:36 +0800

    update upload method

--- README.md
+++ README.md
@@ -3,20 +3,43 @@
 Pack all installed package in ./node_modules, and publish to Nexus hosted Registry.
 
 ## Install
-
 ```sh
 npm install -g @fanwang/pack-util
 ```
 
 ## Usage: pack-util [command]
-
+如果使用控制台命令的时候提示电脑禁止运行脚本,用管理员身份运行 cmd 或者 vscode 等然后在命令行输入:
+```
+Set-ExecutionPolicy RemoteSigned
+```
 ### Pack all installed package in ./node_modules
+将所有 ./node_modules 中安装的包打包至 ./node_modules_pack
 ```sh
 cd your_project
 pack-util pack
 ```
 
-###  Publish all packed packages in ./node_modules_pack
+### Upload all packed packages in ./node_modules_pack
+1. Copy ./node_modules_pack along with your project.
+将 ./node_modules_pack 文件夹和你的项目一起拷贝至内网，然后执行上传。
+```sh
+cd your_project
+pack-util upload
+```
+2. Input some params
+输入参数
+```
+? Please input your Nexus url host (eg: http://xx.xx.xx.xx:xxxx):  http://127.0.0.1:8081/
+? Please input your Nexus npm repository (eg: localNpm):  localNpm
+? login to Nexus by username:  admin
+? password:  [hidden]
+```
+3. If there is upload error, delete other packages within the ./node_modules_pack, and upload again.
+如果出现上传失败，删除其他成功上传的包，然后重新执行上传。
+
+###  ~~Publish all packed packages in ./node_modules_pack~~
+npm publish 会将解析打包的 package.json 中 publishConfig, 有的公共包如果设置了 { "registry": "https://registry.npmjs.org/" } , 会导致publish失败。目前[没法 override npm 的这种行为](https://stackoverflow.com/questions/66914753/override-registry-mentioned-in-publishconfig-of-package-json-through-command-lin)。
+
 copy node_modules_pack along with your project.
 1. change registry 
 ```sh
@@ -31,3 +54,7 @@ npm adduser -registry http://xx.xx.xx.xx:xxxx/repository/xxxx/
 cd your_project
 pack-util publish
 ```
+~~
+
+## TODO
+- [] 实现打包和上传显示进度
--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,60 @@
 #!/usr/bin/env node
 
 import { execa } from 'execa'
-import { mkdirSync, readdirSync } from 'fs'
+import { mkdirSync, readdirSync, createReadStream } from 'fs'
 import { Command } from 'commander'
+import inquirer from 'inquirer'
+import FormData from 'form-data'
 
 const program = new Command()
+
+program
+  .name('pack-util')
+  .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
+  .version('1.0.6')
 
 const getPackageNames = async () => {
   const { stdout } = await execa('npm', ['list', '--all'])
   let packages = stdout.split('\n')
-    .filter(item => !item.includes('deduped'))//去掉重复包
-    .map(name => name.substring(name.lastIndexOf(' ') + 1))//解析每行的包名称
-    .filter(packageName => packageName.length > 0)//去掉空行
   packages.splice(0, 1)//去掉首行路径字符串
-  return packages
+  return packages.filter(item => !item.includes('deduped'))//去掉重复包
+    .map(name => {
+      const nameRegex = /(\S+@.+)/g
+      const nameParse = name.match(nameRegex)
+      if (nameParse && nameParse.length > 0) return nameParse[0]
+      else return ''
+    })//解析每行的包名称
+    .filter(packageName => packageName.length > 0)//去掉空行
 }
 
+// 利用 npm pack 在外网上对 dependencies 中的所有包打包。
 const packPackage = async (packageName) => {
   const { stdout } = await execa('npm', ['pack', packageName, '--pack-destination=./node_modules_pack'])
   return stdout
 }
 
+// npm publish 会将解析打包的 package.json 中 publishConfig, 有的公共包如果设置了 { "registry": "https://registry.npmjs.org/" } , 会导致publish失败。
 const publishPackage = async (packageName) => {
   const { stdout } = await execa('npm', ['publish', './node_modules_pack/' + packageName])
   return stdout
 }
 
-program
-  .name('pack-util')
-  .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
-  .version('1.0.4')
+// 通过 nexus 的 components upload API 上传 npm 包，有时候会报 statusCode 500，暂不知道原因，重新上传即可。
+const uploadPackage = async (packageName, options) => {
+  const { host, repository, username, password } = options
+  const form = new FormData()
+  form.append('npm.asset', createReadStream('./node_modules_pack/' + packageName))
+  // return form
+  form.submit({
+    hostname: new URL(host).hostname,
+    port: new URL(host).port,
+    path: `/service/rest/v1/components?repository=${repository}`,
+    auth: `${username}:${password}`
+  }, function (err, res) {
+    if (res.statusCode > 200 && res.statusCode < 300) console.log(packageName)
+    else console.log(`${packageName} : UPLOAD ERROR -- code ${res.statusCode} -- ${res.statusMessage}`)
+  })
+}
 
 program
   .command('pack')
@@ -39,19 +64,59 @@ program
       console.log(`Total ${packages.length} packages to be packed, please wait...`)
       mkdirSync('./node_modules_pack', { recursive: true })
       packages.forEach(packageName => {
-        packPackage(packageName).then(stdout => console.log(stdout))
+        packPackage(packageName).then(stdout => {
+          console.log(stdout)
+        })
       })
     })
   })
 
+
 program
-  .command('publish')
-  .description('Publish all packed packages in ./node_modules_pack')
+  .command('upload')
+  .description('Upload all packed packages in ./node_modules_pack to Nexus self-hosted Registry')
   .action(() => {
-    const packages = readdirSync('./node_modules_pack')
-    packages.forEach(packageName => {
-      publishPackage(packageName).then(stdout => console.log(stdout))
+    const questions = [
+      {
+        type: 'input',
+        name: 'host',
+        message: 'Please input your Nexus url host (eg: http://xx.xx.xx.xx:xxxx): '
+      },
+      {
+        type: 'input',
+        name: 'repository',
+        message: 'Please input your Nexus npm repository (eg: localNpm): ',
+        default: 'localNpm'
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'login to Nexus by username: ',
+        default: 'admin'
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'password: '
+      },
+    ]
+    inquirer.prompt(questions).then(options => {
+      const packages = readdirSync('./node_modules_pack')
+      console.log(`Total ${packages.length} packages to be uploaded, please wait...`)
+      packages.forEach(packageName => {
+        uploadPackage(packageName, options)
+      })
     })
   })
+
+// program
+//   .command('publish')
+//   .description('Publish all packed packages in ./node_modules_pack')
+//   .action(() => {
+//     const packages = readdirSync('./node_modules_pack')
+//     packages.forEach(packageName => {
+//       publishPackage(packageName).then(stdout => console.log(stdout))
+//     })
+//   })
 
 program.parse()
