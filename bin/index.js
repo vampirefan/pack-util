312c897a190dc32d14abd21448ec571dc4c09136
vampirefan <vampirefan@126.com>
2023-10-12 08:27:06 +0800

    chore: release v2.0.3

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,7 @@ const program = new Command()
 program
   .name('pack-util')
   .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
-  .version('2.0.2')
+  .version('2.0.3')
 
 const getPackageInfos = async (packageManager) => {
   if (packageManager === 'yarn') {
