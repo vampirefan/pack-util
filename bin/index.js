ce38ae63b175d9ba7783f44a862d4346024fffd0
vampirefan <vampirefanmail@gmail.com>
2024-10-09 16:09:42 +0800

    chore: release v3.1.0

--- bin/index.js
+++ bin/index.js
@@ -15,7 +15,7 @@ const program = new Command()
 program
   .name('pack-util')
   .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
-  .version('3.0.1')
+  .version('3.1.0')
 
 
 const limit = pLimit(10)
