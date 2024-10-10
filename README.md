cf0e97f3aa38d3f1d073cbf1d7a52d057f7cb225
vampirefan <vampirefanmail@gmail.com>
2024-10-10 08:36:19 +0800

    feat: download peerDependencies for pnpm method

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# pack-util  
+# pack-util
 
-**中文** | [English](./README.en-US.md)  
+**中文** | [English](./README.en-US.md)
 
 一个打包工具，可以在互联网网机器上将项目 ./node_modules 中安装的包遍历后打包，并放在 ./node_modules_pack 文件夹中，并批量上传到 nexus 搭建的 npm 私服上。支持 `pnpm`/`yarn`/`npm` 打包工具。
 
-## 安装  
+## 安装
 
 全局安装即可。
 ```sh
 npm install -g @fanwang/pack-util
 ```
 
-## 使用  
+## 使用
 
-**使用说明**  
+**使用说明**
 
 如果使用控制台命令的时候提示电脑禁止运行脚本,用管理员身份运行 cmd 或者 vscode 等然后在命令行输入:
 
@@ -23,11 +23,11 @@ Set-ExecutionPolicy RemoteSigned
 
 建议先将源仓库设为阿里镜像仓库，这样打包速度更快（yarn 和 npm 需要同时设置）：
 ```sh
-yarn config set registry https://registry.npmmirror.com  
-npm config set registry https://registry.npmmirror.com  
+yarn config set registry https://registry.npmmirror.com
+npm config set registry https://registry.npmmirror.com
 ```
 
-### 第一步. 将项目所有依赖包下载至 ./node_modules_pack 文件夹  
+### 第一步. 将项目所有依赖包下载至 ./node_modules_pack 文件夹
 ```sh
 cd your_project
 pack-util pack
@@ -41,8 +41,8 @@ pack-util pack
 
 因此，如果单纯的想从互联网下载一些包，可以仅凭一个 package.json 文件，利用不同的包管理工具执行 install 得到相应的检索文件后，就可以执行 pack 操作了。甚至建立一个空文件夹，然后通过 `yarn add` | `pnpm add` | `npm install` 添加自己需要的包来直接得到检索文件，也是可以的。
 
-### 第二步. 将依赖包批量上传到 nexus 搭建的 npm 私服上  
-1. 将 ./node_modules_pack 文件夹和你的项目一起拷贝至内网机器上，然后执行上传。  
+### 第二步. 将依赖包批量上传到 nexus 搭建的 npm 私服上
+1. 将 ./node_modules_pack 文件夹和你的项目一起拷贝至内网机器上，然后执行上传。
 ```sh
 cd your_project
 pack-util upload
@@ -50,7 +50,7 @@ pack-util upload
 **解释：**
 工具是在命令执行目录下查找 ./node_modules_pack 文件夹，然后把文件夹中的包批量上传，所以只需要把 ./node_modules_pack 文件夹拷贝到命令执行目录即可。
 
-2. 输入参数  
+2. 输入参数
 ```
 ? Please input your Nexus url host (eg: http://xx.xx.xx.xx:xxxx): http://xx.xx.xx.xx:8081/
 ? Please input your Nexus npm repository (eg: localNpm):  localNpm
@@ -58,26 +58,25 @@ pack-util upload
 ? password:  [hidden]
 ```
 
-3. 如果出现上传失败，删除其他成功上传的包，然后重新执行上传（也可以直接重试上传）。  
+3. 如果出现上传失败，删除其他成功上传的包，然后重新执行上传（也可以直接重试上传）。
 
-### 第三步. 在内网机器上执行包依赖安装  
+### 第三步. 在内网机器上执行包依赖安装
 先删除 `yarn.lock` / `package-lock.json` / `pnpm-lock.yaml` 文件。
 
-**PNPM**  
+**PNPM**
 ```
 pnpm config set registry http://xx.xx.xx.xx:xxxx/repository/localNpm/
 pnpm install
 ```
 
-**YARN**  
+**YARN**
 ```
 yarn config set registry http://xx.xx.xx.xx:xxxx/repository/localNpm/
 yarn install
 ```
 
-**NPM**  
+**NPM**
 ```
 npm config set registry http://xx.xx.xx.xx:xxxx/repository/localNpm/
 npm install --save --legacy-peer-deps  # 注意：在私网上安装 npm 包需要忽略对 peer 依赖
 ```
-
--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-import { execa } from 'execa'
-import { mkdirSync, readdirSync, createReadStream, createWriteStream, readFileSync } from 'fs'
+import { createReadStream, createWriteStream, mkdirSync, readdirSync, readFileSync } from 'node:fs'
+import process from 'node:process'
+import url from 'node:url'
 import { Command } from 'commander'
-import inquirer from 'inquirer'
+import { execa } from 'execa'
 import FormData from 'form-data'
+import inquirer from 'inquirer'
 import fetch from 'node-fetch'
-import url from 'url'
-import yaml from 'yaml'
 import pLimit from 'p-limit'
+import yaml from 'yaml'
 
 const program = new Command()
 
@@ -17,33 +18,50 @@ program
   .description('Pack all installed package in ./node_modules, and publish to Nexus self-hosted Registry.')
   .version('3.1.0')
 
-
 const limit = pLimit(10)
-const getPackageInfos = async (packageManager) => {
+async function getPackageInfos(packageManager) {
   // 使用 yarn
   if (packageManager === 'yarn') {
     const fileData = readFileSync('./yarn.lock')
-    return fileData.toString().split(/\r?\n/)
-      .filter(line => line.startsWith('  resolved'))
-      .map(line => line.substring(12, line.length - 1))
+    return fileData.toString().split(/\r?\n/).filter(line => line.startsWith('  resolved')).map(line => line.substring(12, line.length - 1))
   }
+  // 使用 pnpm
   else if (packageManager === 'pnpm') {
     const fileData = readFileSync('./pnpm-lock.yaml', 'utf8')
     const pnpmLock = yaml.parse(fileData)
-    // 直接使用 `packages` 下的键作为包名和版本
-    const packages = Object.keys(pnpmLock.packages)
-    return Array.from(new Set(packages)) // 去掉重复项
+    // 直接使用 `packages` 下的键作为包名和版本，同时处理其下的 `peerDependencies`
+    const packages = Object.keys(pnpmLock.packages).reduce((acc, key) => {
+      const pkg = pnpmLock.packages[key]
+      // 将当前包加入列表
+      acc.add(key)
+      if (pkg.peerDependencies) {
+        Object.keys(pkg.peerDependencies).forEach((peer) => {
+          const peerVersion = pkg.peerDependencies[peer]
+          // 只匹配版本号
+          const versionRegex = /(\d+\.\d+\.\d+)/
+          const versionMatch = peerVersion.match(versionRegex)
+          if (versionMatch && versionMatch[1]) {
+            const exactVersion = versionMatch[1]
+            const peerPackageKey = `${peer}@${exactVersion}`
+            acc.add(peerPackageKey)
+          }
+        })
+      }
+      return acc
+    }, new Set())
+    return Array.from(packages) // 返回去重后的包
   }
   // 使用 npm
   else {
     const { stdout } = await execa('npm', ['list', '--all'])
     let packages = stdout.split('\n')
-    packages.splice(0, 1) //去掉首行路径字符串
+    packages.splice(0, 1) // 去掉首行路径字符串
     packages = packages.filter(item => !item.includes('deduped')) // 去掉标记重复的包
-      .map(name => {
+      .map((name) => {
         const nameRegex = /(\S+@.+)/g
         const nameParse = name.match(nameRegex)
-        if (nameParse && nameParse.length > 0) return nameParse[0]
+        if (nameParse && nameParse.length > 0)
+          return nameParse[0]
         else return ''
       }) // 解析每行的包名称
       .filter(packageName => packageName.length > 0) // 去掉空行
@@ -51,8 +69,7 @@ const getPackageInfos = async (packageManager) => {
   }
 }
 
-
-var index = 0
+let index = 0
 // 利用 npm pack 在外网上对 dependencies 中的所有包打包。
 // const packPackage = async (packageName) => {
 //   const { stdout } = await execa('npm', ['pack', packageName, '--pack-destination=./node_modules_pack'])
@@ -65,21 +82,23 @@ var index = 0
 // }
 
 // 直接利用依赖包的下载地址进行下载
-const downloadPackage = async (packageUrl) => {
-  const urlObj = url.parse(packageUrl)
+async function downloadPackage(packageUrl) {
+  const urlObj = new url.URL(packageUrl)
   const filename = urlObj.pathname.substring(urlObj.pathname.lastIndexOf('/') + 1)
-  const fileStream = createWriteStream('./node_modules_pack/' + filename)
+  const fileStream = createWriteStream(`./node_modules_pack/${filename}`)
   let res
   try {
     res = await fetch(packageUrl)
-  } catch (error) {
-    console.error(`Failed to download package ${packageUrl}: ${error.message}`)
+  }
+  catch (error) {
+    console.error(`\nWARN: Failed to download package ${packageUrl}: ${error.message}`)
     return filename
   }
   if (!res?.ok) {
-    console.error(`Failed to download package ${filename}: ${res.status} ${res.statusText}`)
+    console.error(`\nWARN: Failed to download package ${filename}: ${res.status} ${res.statusText}`)
     return filename
-  } else {
+  }
+  else {
     res.body.pipe(fileStream)
     await new Promise((resolve, reject) => {
       fileStream.on('finish', () => {
@@ -97,17 +116,19 @@ const downloadPackage = async (packageUrl) => {
 }
 
 // 通过 nexus 的 components upload API 上传 npm 包，有时候会报 statusCode 500，暂不知道原因，重新上传即可。
-const uploadPackage = async (packageName, options) => {
+async function uploadPackage(packageName, options) {
   const { host, repository, username, password } = options
   const form = new FormData()
-  form.append('npm.asset', createReadStream('./node_modules_pack/' + packageName))
+  form.append('npm.asset', createReadStream(`./node_modules_pack/${packageName}`))
   form.submit({
     hostname: new URL(host).hostname,
     port: new URL(host).port,
     path: `/service/rest/v1/components?repository=${repository}`,
-    auth: `${username}:${password}`
-  }, function (err, res) {
-    if (err) console.log(err.message)
+    auth: `${username}:${password}`,
+  }, (err, res) => {
+    if (err) {
+      process.stdout.write(`\n${err.message}`)
+    }
     else {
       if (res.statusCode > 200 && res.statusCode < 300) {
         // upload success
@@ -116,7 +137,9 @@ const uploadPackage = async (packageName, options) => {
         process.stdout.cursorTo(0)
         process.stdout.write(`${index}: ${packageName}`) // 在同一行打印处理进度
       }
-      else console.log(`${packageName} : UPLOAD ERROR -- code ${res.statusCode} -- ${res.statusMessage}`) //打印上传失败的包
+      else {
+        process.stdout.write(`\n${packageName} : UPLOAD ERROR -- code ${res.statusCode} -- ${res.statusMessage}`)
+      } // 打印上传失败的包
     }
   })
 }
@@ -131,36 +154,36 @@ program
         name: 'packageManager',
         message: 'Please select your package manager:',
         choices: ['pnpm', 'yarn', 'npm'],
-        default: 'pnpm'
+        default: 'pnpm',
       },
     ]
     inquirer.prompt(questions).then((options) => {
       getPackageInfos(options.packageManager).then(async (packages) => {
-        console.log(`Total ${packages.length} packages to be packed, please wait...`)
+        process.stdout.write(`Total ${packages.length} packages to be packed, please wait...\n`)
         mkdirSync('./node_modules_pack', { recursive: true })
         if (options.packageManager === 'yarn') {
-          const downloadTasks = packages.map((packageUrl) =>
-            limit(() => downloadPackage(packageUrl))
+          const downloadTasks = packages.map(packageUrl =>
+            limit(() => downloadPackage(packageUrl)),
           )
           await Promise.all(downloadTasks)
-        } else {
+        }
+        else {
           const { stdout } = await execa('npm', ['config', 'get', 'registry'])
-          const registryUrl = stdout.endsWith('/') ? stdout : `${stdout}/`;
+          const registryUrl = stdout.endsWith('/') ? stdout : `${stdout}/`
           const packTasks = packages.map((packageName) => {
             const atIndex = packageName.lastIndexOf('@')
             const fullname = packageName.substring(0, atIndex)
             const name = fullname.includes('/') ? fullname.split('/')[1] : fullname
             const version = packageName.substring(atIndex + 1)
-            const packageUrl = `${registryUrl}${fullname}/-/${name}-${version}.tgz`;
+            const packageUrl = `${registryUrl}${fullname}/-/${name}-${version}.tgz`
             return limit(() => downloadPackage(packageUrl))
           })
           await Promise.all(packTasks)
         }
-        console.log('\nAll packages are successfully packed in ./node_modules_pack!')
+        process.stdout.write('\nAll packages are successfully packed in ./node_modules_pack!')
       })
     })
   })
-
 
 program
   .command('upload')
@@ -170,29 +193,29 @@ program
       {
         type: 'input',
         name: 'host',
-        message: 'Please input your Nexus url host (eg: http://xx.xx.xx.xx:xxxx): '
+        message: 'Please input your Nexus url host (eg: http://xx.xx.xx.xx:xxxx): ',
       },
       {
         type: 'input',
         name: 'repository',
         message: 'Please input your Nexus npm repository (eg: localNpm): ',
-        default: 'localNpm'
+        default: 'localNpm',
       },
       {
         type: 'input',
         name: 'username',
         message: 'login to Nexus by username: ',
-        default: 'admin'
+        default: 'admin',
       },
       {
         type: 'password',
         name: 'password',
-        message: 'password: '
+        message: 'password: ',
       },
     ]
-    inquirer.prompt(questions).then(options => {
+    inquirer.prompt(questions).then((options) => {
       const packages = readdirSync('./node_modules_pack')
-      console.log(`Total ${packages.length} packages to be uploaded, please wait...`)
+      process.stdout.write(`\nTotal ${packages.length} packages to be uploaded, please wait...`)
       for (const packageName of packages) {
         uploadPackage(packageName, options)
       }
